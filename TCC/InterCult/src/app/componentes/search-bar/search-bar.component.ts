afe5736d3e7608f60d9cabc85f850b56ba7171df
andre14souza <andre14souzza@gmail.com>
2024-10-07 20:47:33 -0300

    toma ai a tela de programas vapo

--- TCC/InterCult/src/app/componentes/search-bar/search-bar.component.ts
+++ TCC/InterCult/src/app/componentes/search-bar/search-bar.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SearchBarComponent {
 
+  locais: string[] = ['Todos','EUA', 'Itália', 'França', 'Irlanda', 'Austrália'];
+
+  agencias: string[] = ['Todas','EF', 'CI'];
+
+  precos: string[] = ['Qualquer preço','R$ 1.500 a R$ 5.000','R$ 5.000 a R$ 15.000', 'Mais de R$ 15.000'];
+
 }
--- TCC/InterCult/src/app/pages/results/results.component.ts
+++ TCC/InterCult/src/app/pages/results/results.component.ts
@@ -6,11 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './results.component.css'
 })
 export class ResultsComponent {
-  cursos = [
-    { titulo: 'Programa/Curso', preco: 6399.00, pais: 'Nova Zelândia', cidade: 'Cidade', avaliacao: 4.9 },
-    { titulo: 'Programa/Curso', preco: 6399.00, pais: 'Nova Zelândia', cidade: 'Cidade', avaliacao: 4.9 },
-    { titulo: 'Programa/Curso', preco: 6399.00, pais: 'Nova Zelândia', cidade: 'Cidade', avaliacao: 4.9 },
-    { titulo: 'Programa/Curso', preco: 6399.00, pais: 'Nova Zelândia', cidade: 'Cidade', avaliacao: 4.9 }
+  resultado = [
+    { nome: 'Curso de Inglês Geral', agencia:'EF', preco: 5350.00, pais: 'Nova Zelândia', cidade: 'Auckland', avaliacao: 4.4 },
+    { nome: 'Curso de Francês Intensivo',agencia:'EF', preco: 2555.00, pais: 'França', cidade: 'Paris', avaliacao: 4.7 },
+    { nome: 'High School', agencia:'CI', preco: 36000.00, pais: 'EUA', cidade: 'Los Angeles, Califórnia', avaliacao: 4.9 },
+    { nome: 'Curso de Idiomas na Austrália', agencia:'CI', preco: 6800.00, pais: 'Austrália', cidade: 'Sydney', avaliacao: 4.9 }
   ];
+
+  tipos: string[] = ['Curso de Idioma', 'Teen', 'High School', 'Trabalhar e Estudar Idioma'];
+
+  // Definindo os países disponíveis para filtro
+  paises: string[] = ['Canadá', 'Espanha', 'Estados Unidos', 'Inglaterra', 'Nova Zelândia'];
+
+  // O que está incluso no curso
+  opcoes: string[] = ['Acomodações', 'Casa de família', 'Residência Estudantil'];
+
+  filtros = {
+    tipo: [],
+    pais: [],
+    incluso: [],
+    avaliacaoMin: 0
+  };
+
 
 }
