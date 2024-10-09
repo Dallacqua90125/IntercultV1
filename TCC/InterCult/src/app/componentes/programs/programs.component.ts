1c6c7564f672f2dd162621d96301fbfd495c4416
andre14souza <andre14souzza@gmail.com>
2024-10-09 09:47:53 -0300

    carrossel (quase finalizado)

--- TCC/InterCult/src/app/componentes/programs/programs.component.ts
+++ TCC/InterCult/src/app/componentes/programs/programs.component.ts
@@ -1,4 +1,3 @@
-// programs.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ContentService } from '../../services/content/content.service';
 import { Content } from '../../models/Content';
@@ -6,19 +5,37 @@ import { Content } from '../../models/Content';
 @Component({
   selector: 'app-programs',
   templateUrl: './programs.component.html',
-  styleUrl: './programs.component.css' 
+  styleUrls: ['./programs.component.css']
 })
 export class ProgramsComponent implements OnInit {
-
-  Contents: Content[] = []; 
+  Contents: Content[] = [];  // Todos os itens
+  visibleContents: Content[] = []; // Itens visíveis no carrossel
+  currentIndex: number = 0; // Índice atual
+  visibleCount: number = 3; // Número de itens a serem exibidos por vez
 
   constructor(private contentService: ContentService) {}
 
   ngOnInit(): void {
-    
     this.contentService.GetContent().subscribe(data => {
-      const dados = data.data; 
-      this.Contents = dados; 
+      const dados = data.data; // Supondo que os dados estão em 'data.data'
+      this.Contents = dados;
+      this.updateVisibleContents(); // Atualiza os itens visíveis na inicialização
     });
+  }
+
+  updateVisibleContents() {
+    // Atualiza a lista de itens visíveis limitando a 3
+    
+    this.visibleContents = this.Contents.slice(this.currentIndex, this.currentIndex + this.visibleCount);
+    console.log(this.visibleContents)
+  }
+  
+  moveCarousel(direction: string) {
+    if (direction === 'next' && this.currentIndex + this.visibleCount < this.Contents.length) {
+      this.currentIndex += this.visibleCount; // Avança
+    } else if (direction === 'prev' && this.currentIndex > 0) {
+      this.currentIndex -= this.visibleCount; // Volta
+    }
+    this.updateVisibleContents(); // Atualiza a lista de itens visíveis
   }
 }
