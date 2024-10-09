ff471a278dc23c29873023580dca47bfb382908a
duduzinmilgrau <eduardobr1612@gmail.com>
2024-10-08 22:11:54 -0300

    tela de agencias pt2 -edu

--- TCC/InterCult/src/app/pages/agencias/agencias.component.ts
+++ TCC/InterCult/src/app/pages/agencias/agencias.component.ts
@@ -7,26 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgenciasComponent implements OnInit {
   searchText: string = '';
-  imgEF: string =  'assets/ef.png';
-  imgCI: string =  'assets/ci.png';
   agencias = [
     {
       nome: 'EF',
       paises: '🇯🇵 🇰🇷 🇺🇦 🇺🇸 🇫🇷 🇮🇹 🇪🇸 🇬🇧',
       rating: '★★★★☆',
       avaliacoes: '4.5',
-      img: 'assets/ef-logo.png'
+      img: 'assets/ef-logo.png',
+      link: 'https://www.ef.com.br'  // Link para a agência EF
     },
     {
       nome: 'CI',
       paises: '🇿🇦 🇺🇸 🇬🇧 🇦🇴',
       rating: '★★★★☆',
       avaliacoes: '4.5',
-      img: 'assets/ci - logo.png'
+      img: 'assets/ci - logo.png',
+      link: 'https://www.ci.com.br/pt-br/home'  // Link para a agência CI
     }
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  navigateTo(nome: string): void {
+    const agencia = this.agencias.find(agencia => agencia.nome === nome);
+    if (agencia && agencia.link) {
+      window.open(agencia.link, '_blank'); // Abre o link em uma nova aba
+    }
+  }
 }
